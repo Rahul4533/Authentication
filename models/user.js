2df0f7ef4cb972bb0bab87531eb653cc29ca3fd4
Rahul4533 <106775929+Rahul4533@users.noreply.github.com>
2023-08-01 22:46:14 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -19,6 +19,6 @@ const userSchema=new mongoose.Schema({
 })
 
 // creating model of user
-const user= mongoose.model('users',userSchema)
+const user= mongoose.model('user',userSchema)
 
 module.exports=user;
