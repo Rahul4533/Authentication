3faab793d86863b1ecacadd8621099f938e9de78
Apple <apple@Apples-MacBook-Air.local>
2023-08-24 00:25:37 +0530

    update

--- Router/index.js
+++ Router/index.js
@@ -14,7 +14,7 @@ router.post('/signup',HomeController.create);
 
 router.get('/forget',HomeController.forget);
 
-router.post('/login',passport.authenticate('local', { failureRedirect: '/signup' }),middle.checkAuth, HomeController.login);
+router.post('/login',passport.authenticate('local', { failureRedirect: '/signup' }), HomeController.login);
 
 router.get('/auth' , passport.authenticate('google', { scope:
     [ 'email', 'profile' ]
